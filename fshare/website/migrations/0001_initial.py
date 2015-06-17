583a4c7592bb8f2edc891735f3eae9ff9119a676
Franck De Goër <degoerdf@ensimag.fr>
2015-06-17 20:01:42 +0200

    Fix bug on migration

--- fshare/website/migrations/0001_initial.py
+++ fshare/website/migrations/0001_initial.py
@@ -15,10 +15,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='File',
             fields=[
-                ('id', models.CharField(primary_key=True, db_index=True, serialize=False, max_length=14, unique=True)),
-                ('title', models.CharField(blank=True, null=True, max_length=255)),
-                ('private_label', models.CharField(blank=True, null=True, max_length=255)),
-                ('description', models.TextField(blank=True, default='', null=True)),
+                ('id', models.CharField(max_length=14, primary_key=True, unique=True, serialize=False, db_index=True)),
+                ('title', models.CharField(max_length=255, null=True, blank=True)),
+                ('private_label', models.CharField(max_length=255, null=True, blank=True)),
+                ('description', models.TextField(null=True, blank=True, default='')),
                 ('path', models.CharField(max_length=1024)),
                 ('checksum', models.CharField(max_length=128)),
                 ('size', models.IntegerField()),
@@ -27,11 +27,44 @@ class Migration(migrations.Migration):
                 ('nb_hits', models.IntegerField(default=0)),
                 ('nb_dl', models.IntegerField(default=0)),
                 ('is_private', models.BooleanField(default=False)),
-                ('pwd_hash', models.CharField(blank=True, null=True, max_length=512)),
+                ('pwd_hash', models.CharField(max_length=512, null=True, blank=True)),
                 ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'abstract': False,
             },
+        ),
+        migrations.CreateModel(
+            name='FSUser',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Permission',
+            fields=[
+                ('name', models.CharField(max_length=255, primary_key=True, serialize=False)),
+                ('storage_limit', models.IntegerField(default=100000)),
+                ('base_path', models.CharField(max_length=1024)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='RegistrationKey',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('key', models.CharField(max_length=100)),
+                ('used', models.BooleanField(default=False)),
+                ('permission', models.ForeignKey(to='website.Permission')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='fsuser',
+            name='permission',
+            field=models.ForeignKey(to='website.Permission'),
+        ),
+        migrations.AddField(
+            model_name='fsuser',
+            name='user',
+            field=models.OneToOneField(to=settings.AUTH_USER_MODEL),
         ),
     ]
