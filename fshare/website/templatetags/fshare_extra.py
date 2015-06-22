c8d0a5044ff2c2c44ac64caf151fda45b73e7b84
Franck De Goër <degoerdf@ensimag.fr>
2015-06-22 14:02:19 +0200

    Add filter tag for human-readable size

---
+++ fshare/website/templatetags/fshare_extra.py
@@ -0,0 +1,10 @@
+from django import template
+register = template.Library()
+
+@register.filter(name='human_size')
+def human_size(size):
+    unit = ["", "K", "M", "G"]
+    idx = 1
+    while idx < len(unit) and size > (1024 ** (idx + 1)):
+        idx += 1
+    return str(size // (1024 ** idx)) + unit[idx]
