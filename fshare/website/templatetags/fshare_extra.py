c1be6916034d043612696bbc1434576d704c3880
Franck De Goër <degoerdf@ensimag.fr>
2015-07-03 22:08:28 +0200

    Fix bug on human-size conversion filter

--- fshare/website/templatetags/fshare_extra.py
+++ fshare/website/templatetags/fshare_extra.py
@@ -4,7 +4,7 @@ register = template.Library()
 @register.filter(name='human_size')
 def human_size(size):
     unit = ["", "K", "M", "G"]
-    idx = 1
+    idx = 0
     while idx < len(unit) and size > (1024 ** (idx + 1)):
         idx += 1
     return str(size // (1024 ** idx)) + unit[idx]
